3fbce35c41ebfe701d55a5155bbc83780f0435af
Sasha <dimurchik@ukr.net>
2013-12-02 20:29:11 +0200

    ajax через метод $.ajax

--- js/lessons14.js
+++ js/lessons14.js
@@ -2,11 +2,22 @@
  * Created by sasha on 02.12.13.
  */
 ;(function($){
-	$.post(
-			'text.txt',
+	$.ajax(
 			{
-				name: 'sasha',
-				age: 22
+				url: 'text.txt',       //обезательный параметр
+				type: 'GET',
+				date: {},
+				dataType: "text",       //json, text......
+				success: function(){
+					
+				},
+				error: function(){
+
+				},
+				complete: function(){  //выплниться в любом случае
+
+				}
+
 			},
 			function(result){
 		console.log(result);
